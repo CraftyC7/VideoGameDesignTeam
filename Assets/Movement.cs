5b018e88fc0ce3b632d54addb5bd2cb0dba08abc
CraftyC7 <78454072+CraftyC7@users.noreply.github.com>
2025-01-26 12:32:01 -0500

    Movement Test

---
+++ Assets/Movement.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float _speed;
+    private float horizontalDir;
+
+    private Rigidbody2D _rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalDir = (Input.GetKey(KeyCode.D) ? 1 : 0) - (Input.GetKey(KeyCode.A) ? 1 : 0);
+        _rb.linearVelocity = new Vector2(horizontalDir * _speed * Time.deltaTime, _rb.linearVelocity.y);
+    }
+
+}
