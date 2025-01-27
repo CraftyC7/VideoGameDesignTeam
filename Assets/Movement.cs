331761f391b77c3ae1287e524bb69b82b7619abf
CraftyC7 <78454072+CraftyC7@users.noreply.github.com>
2025-01-27 17:50:14 -0500

    Basic Movement

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -7,8 +7,8 @@ public class Movement : MonoBehaviour
 {
     public float _speed;
     private float horizontalDir;
-
     private Rigidbody2D _rb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
