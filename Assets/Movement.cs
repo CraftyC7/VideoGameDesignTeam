9088b7ae614a0f09fa7df5c32619c514955ac8b0
CraftyC7 <78454072+CraftyC7@users.noreply.github.com>
2025-02-01 14:54:13 -0500

    Continued Horizontal Movement

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -5,23 +5,44 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed = 5;
-    private Vector2 _velocity;
+    public float topSpeed = 5f;
+    public float jumpForce = 5f;
+    public float moveAcc = 0.2f;
+    public float movePower = 1.1f;
+    private float accel;
+    private float decel;
+    private bool _jump;
+    private float _moveDir;
     private Rigidbody2D _rb;
 
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        accel = moveAcc;
+        decel = -moveAcc;
     }
 
     void Update()
     {
-        _velocity = new Vector2(Input.GetAxis("Horizontal") * speed, 0);
+        _moveDir = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && !_jump) {
+            _rb.linearVelocity = new Vector2(_rb.linearVelocityX, jumpForce);
+            _jump = true;
+        }
     }
 
     private void FixedUpdate()
     {
-        _rb.linearVelocity = new Vector2(_velocity.x, _rb.linearVelocityY + _velocity.y);
+        float maxSpeed = topSpeed * _moveDir;
+        float potSpeed = maxSpeed - _rb.linearVelocityX;
+        float acceleration = (Mathf.Abs(maxSpeed) > 0.01f) ? accel : decel;
+        float force = Mathf.Pow(Mathf.Abs(potSpeed) * acceleration, movePower) * Mathf.Sign(potSpeed);
+        _rb.AddForce(force * Vector2.right);
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _jump = false;
+    }
 }
