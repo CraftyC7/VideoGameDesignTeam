26baa0b727bc5163f0f1202184f18cebb8b22ea6
CraftyC7 <78454072+CraftyC7@users.noreply.github.com>
2025-02-08 21:50:10 -0500

    Movement and Wall Jump
    Wall jumps added and movement improvements.

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -2,6 +2,7 @@ using System;
 using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Movement : MonoBehaviour
 {
@@ -10,10 +11,19 @@ public class Movement : MonoBehaviour
     public float moveAcc = 0.2f;
     public float movePower = 1.1f;
     public float friction = 3f;
+    public float jumpTime = 0.5f;
+    public float wallSlide = 0.2f;
+    public float wallPushForce = 5f;
+    private float airTime = 0f;
     private float accel;
     private float decel;
+    private bool _onFloor;
+    private bool _onWall;
     private bool _jump;
+    private bool _isJumping;
+    private float _timeSinceJump;
     private float _moveDir;
+    private float _wallCollideVelocity;
     private Rigidbody2D _rb;
 
     void Start()
@@ -26,10 +36,33 @@ public class Movement : MonoBehaviour
     void Update()
     {
         _moveDir = Input.GetAxis("Horizontal");
+        _timeSinceJump += Time.deltaTime;
 
-        if (Input.GetButtonDown("Jump") && !_jump) {
-            _rb.linearVelocity = new Vector2(_rb.linearVelocityX, jumpForce);
+        if (_onFloor && _onWall)
+        {
+            _onWall = false;
+        }
+
+        if (Input.GetButtonDown("Jump") && !_jump && Math.Abs(_rb.linearVelocityY) < 0.01)
+        {
             _jump = true;
+            _isJumping = true;
+            _timeSinceJump = 0;
+        }
+        else if (Input.GetButtonDown("Jump") && _onWall && !_onFloor) 
+        {
+            _rb.gravityScale = 2;
+            _onWall = false;
+            _jump = true;
+            _isJumping = true;
+            _timeSinceJump = 0;
+            _rb.linearVelocity = (-Mathf.Sign(_wallCollideVelocity) * wallPushForce) * Vector2.right;
+        }
+
+        if ((!Input.GetButton("Jump") || _timeSinceJump > jumpTime) && _isJumping) {
+            _isJumping = false;
+        } else if (Input.GetButton("Jump") && _timeSinceJump <= jumpTime && _isJumping) {
+            _rb.linearVelocity = new Vector2(_rb.linearVelocityX, jumpForce);
         }
     }
 
@@ -45,14 +78,35 @@ public class Movement : MonoBehaviour
         }
         else
         {
-
             float frictionForce = friction * Mathf.Sign(-_rb.linearVelocityX);
             _rb.AddForce(frictionForce * Vector2.right);
+        }
+
+        if (_onWall && !_onFloor)
+        {
+            _rb.linearVelocity = wallSlide * Vector2.up;
+            _rb.gravityScale = 0f;
+        }
+        else {
+            _rb.gravityScale = 2;
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        _jump = false;
+        if (collision.gameObject.CompareTag("Floor")) { _jump = false; _onFloor = true; }
+        else if (collision.gameObject.CompareTag("Wall")) 
+        {
+            if (!_onWall) {
+                _wallCollideVelocity = collision.relativeVelocity.x;
+            }
+            _onWall = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall")) { _onWall = false; } 
+        else if (collision.gameObject.CompareTag("Floor")) { _onFloor = false; }
     }
 }
